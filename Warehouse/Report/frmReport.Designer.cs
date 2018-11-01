f729aaccb08e558b56cb58f28108d14329f22c1f
Ching <42258732+lcq9732@users.noreply.github.com>
2018-10-31 20:31:24 -0500

    Add files via upload

---
+++ Warehouse/Report/frmReport.Designer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Warehouse.Report
+{
+    partial class frmReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rptViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.btnGeneral = new System.Windows.Forms.Button();
+            this.dtTo = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtFrom = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rptViewer
+            // 
+            this.rptViewer.Location = new System.Drawing.Point(1, 42);
+            this.rptViewer.Name = "rptViewer";
+            this.rptViewer.Size = new System.Drawing.Size(940, 529);
+            this.rptViewer.TabIndex = 0;
+            // 
+            // btnGeneral
+            // 
+            this.btnGeneral.Location = new System.Drawing.Point(335, 12);
+            this.btnGeneral.Name = "btnGeneral";
+            this.btnGeneral.Size = new System.Drawing.Size(75, 23);
+            this.btnGeneral.TabIndex = 11;
+            this.btnGeneral.Text = "生成报表";
+            this.btnGeneral.UseVisualStyleBackColor = true;
+            this.btnGeneral.Click += new System.EventHandler(this.btnGeneral_Click);
+            // 
+            // dtTo
+            // 
+            this.dtTo.Location = new System.Drawing.Point(200, 12);
+            this.dtTo.Name = "dtTo";
+            this.dtTo.Size = new System.Drawing.Size(120, 21);
+            this.dtTo.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "到";
+            // 
+            // dtFrom
+            // 
+            this.dtFrom.Location = new System.Drawing.Point(50, 12);
+            this.dtFrom.Name = "dtFrom";
+            this.dtFrom.Size = new System.Drawing.Size(120, 21);
+            this.dtFrom.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "从：";
+            // 
+            // frmReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 573);
+            this.Controls.Add(this.btnGeneral);
+            this.Controls.Add(this.dtTo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtFrom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rptViewer);
+            this.Name = "frmReport";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "frmReport";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmReport_Load);
+            this.Resize += new System.EventHandler(this.frmReport_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer rptViewer;
+        private System.Windows.Forms.Button btnGeneral;
+        private System.Windows.Forms.DateTimePicker dtTo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtFrom;
+        private System.Windows.Forms.Label label1;
+    }
+}
